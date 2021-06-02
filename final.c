f2be6b71e4479bc927b9ca73f6d2fae8688352ba
Kimchijoa609 <ghdwnsrl1991@gmail.com>
2021-06-03 02:52:37 +0900

    2차

--- final.c
+++ final.c
@@ -6,37 +6,86 @@
 #include <errno.h>
 #define MAX_SIZE 255
 
+int insert(FILE *fp, FILE* fp2 ,int i ,char*buf){
+
+	int count=0;
+
+	for(int j=i; j<strlen(buf)-1;j++){
+             fprintf(fp2,"%c",buf[j]);
+             count++;
+           }
+	return count;
+}
+
+
 void ReadnWrite(FILE * fp ,FILE * fp2){
 	char buf[MAX_SIZE];
 	int count ; // 길이 구하는 칭구
 	int start =0;	
+	int length =0; 
 	while(fgets(buf,sizeof(buf),fp)!=NULL){
 		count =0; //줄마다 검사 후 다시 초기화
-		for(int i = 0; i< strlen(buf)+1;i++){
+		
 
-			if(buf[i]==':'){
-				  i++;	
-					for(int j = i+1 ; j < strlen(buf)-1; j++){		
-						fprintf(fp2,"%c",buf[j]);
-						count++;
-					}
-					fprintf(fp2,"/%d/",count);
-					break;
-			}
+		if(strncmp(buf,"ID:",3)==0){ // id 저장
+ 			printf("ID 발견\n");
+			length=insert(fp,fp2,4,buf);
+			fprintf(fp2,"@%d/",length);
+			continue;
+      }
+		if(strncmp(buf,"NAME:",5)==0){ // 이름 저장
+      printf("이름 발견\n");
+			length=insert(fp,fp2,6,buf);
+      fprintf(fp2,"@%d/",length);
+      continue;
+      }
+
+		if(strncmp(buf,"GENDER:",7)==0){ // 성별 저장
+      printf("성별 발견\n");
+			if(buf[8]='F'){
+			 fprintf(fp2,"F/");
+			}else
+			 fprintf(fp2,"M/");
+      continue;
+    }
+
+		if(strncmp(buf,"AGE:",4)==0){ // 나이 저장
+			printf("나이 발견\n");
+			length=insert(fp,fp2,5,buf);
+			fprintf(fp2,"/");
+			continue;
 		}
-		if(strncmp(buf,"*DESCRIPTION*",13)==0){
-				printf("같아요!\n");
-				start++;
-				continue;
+		if(strncmp(buf,"HP:",3)==0){ // HP 저장
+      printf("HP 발견\n");
+			insert(fp,fp2,4,buf);
+			fprintf(fp2,"/");
+      continue;
+    }
+
+		if(strncmp(buf,"MP:",3)==0){ // MP 저장
+			printf("MP 발견\n");
+      insert(fp,fp2,4,buf);
+      fprintf(fp2,"/");
+      continue;
+		}
+		if(strncmp(buf,"COIN:",5)==0){ // COIN 저장
+      printf("COIN 발견\n");
+      insert(fp,fp2,6,buf);
+      fprintf(fp2,"/");
+      continue;
+     }
+		if(strncmp(buf,"*DESCRIPTION*",13)==0){ // 자기소개 저장
+			printf("같아요!\n");
+			start++;
+			continue;
 		}
 		if(start>0){
 		for(int k=0;k<strlen(buf)-1;k++){
 				fprintf(fp2,"%c",buf[k]);
 				count++;
 		}
-		fprintf(fp2,"/%d/",count);
+		fprintf(fp2,"@%d/",count);
 		}
-
 	}
 	
 }
